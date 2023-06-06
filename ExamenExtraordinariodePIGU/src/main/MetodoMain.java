381180a3120e2083ed52279c45783e7d19b1bcff
Juancarranza02 <79293560+Juancarranza02@users.noreply.github.com>
2023-06-06 14:07:30 -0600

    Primer Commit

---
+++ ExamenExtraordinariodePIGU/src/main/MetodoMain.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+/**
+ *
+ * @author CCNAR
+ */
+public class MetodoMain {
+    
+    public static void main(String[] args) {
+        author63841();
+    }
+    
+    public static void  author63841() {
+        String nombre = "Juan armando Carranza magadan";
+         String matrícula = "63841"; 
+         String correo_electrónico = "al06384@gmail.com";
+         String hobbies[] = {"Leer", "Correr", "Escuchar Musica"};
+        System.out.println("Hola soy "+nombre+" mi Matricula es "+matrícula+"\n Hobbies son : "+"\n *"+hobbies[0]+"\n *"+hobbies[1]+"\n *"+hobbies[2]);
+    }
+}
+
+
